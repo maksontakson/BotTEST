b92785c10aec79dadd585c89c3779f03837acf89
maksi <maksontuskon@gmail.com>
2022-06-15 16:01:49 +0300

    Initial final

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,13 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@SpringBootApplication
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
@@ -17,5 +20,6 @@ public class Application {
             log.error("Failed to register bot(check internet connection / bot token or make sure only one instance of bot is running).", e);
         }
         log.info("Telegram bot is ready to accept updates from user......");
+        SpringApplication.run(Application.class,args);
     }
 }
